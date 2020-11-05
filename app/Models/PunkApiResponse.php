cd2b4efc43ecdda28482ce97ad9b62543aa333cb
Frank Averhoff <faverhofff@gmail.com>
2020-11-05 11:54:02 -0500

    Api response model

---
+++ app/Models/PunkApiResponse.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class PunkApiResponse
+ * @package App\Models
+ * @version November 5, 2020, 1:35 pm UTC
+ *
+ * @property integer $CodeResult
+ * @property string $BodyContent
+ */
+class PunkApiResponse
+{
+    /** @var */
+    private $codeResult;
+
+    /** @var */
+    private  $bodyContent;
+
+    /**
+     * 
+     */
+    public function __construct(int $codeStatus, string $bodyContent)
+    {
+        $this->codeResult = $codeStatus;
+        $this->bodyContent = $bodyContent;
+    }
+
+    /**
+     * 
+     */
+    public function getCode(): int
+    {
+        return $this->codeResult;
+    }
+    
+    /***
+     * 
+     */
+    public function getBodyContent()
+    {
+        return json_decode($this->bodyContent);
+    }
+}
