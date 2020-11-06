adec74b0f067e5dfa3afe5234f8e6d7d440e1269
Frank Averhoff <faverhofff@gmail.com>
2020-11-05 23:17:19 -0500

    Update FilterProductService.php

--- app/Services/FilterProductService.php
+++ app/Services/FilterProductService.php
@@ -6,36 +6,46 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Arr;
 
-class FilterProductService { 
-    
+class FilterProductService
+{
     /**
      * Select from result just the fields
      * ['id', 'name', 'description']
+     * 
+     * @param $arrayPunkApiModel
      */
     public function getSummary($arrayPunkApiModel)
     {
-        return $this->filterArray($arrayPunkApiModel, ['id', 'name', 'description']);
+        return $this->filterArrayByKeys($arrayPunkApiModel, ['id', 'name', 'description']);
     }
 
     /**
      * Select from result just the fields
      * ['id', 'name', 'description']
+     * 
+     * @param array $arrayPunkApiModel
      */
     public function getDetail($arrayPunkApiModel)
     {
         $fields = ['id', 'name', 'description', 'image_url', 'tagline', 'first_brewed'];
 
-        return $this->filterArray($arrayPunkApiModel, $fields);
+        return $this->filterArrayByKeys($arrayPunkApiModel, $fields);
     }
 
     /**
+     * Filter and just leave request fields
      * 
+     * @param array $arrayData
+     * @param array $fieldsToShow
+     * @return array
      */
-    private function filterArray(array $arrayData, array $fieldsToShow)
+    private function filterArrayByKeys(array $arrayData, array $fieldsToShow)
     {
-        return collect($arrayData)->map( function($row) use ($fieldsToShow) {
-            return Arr::only($row, $fieldsToShow);
-        });
+        return collect($arrayData)->map(
+            function ($row) use ($fieldsToShow) {
+                return Arr::only($row, $fieldsToShow);
+            }
+        );
     }
 
 }
